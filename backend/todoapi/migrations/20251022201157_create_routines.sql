d8dbf0757dc189fc89395fc1cd310717ac22e5ed
JackMcGuinness-2 <120398860+JackMcGuinness-2@users.noreply.github.com>
2025-10-22 22:46:04 +0100

    adding exercise tables etc

--- backend/todoapi/migrations/20251022201157_create_routines.sql
+++ backend/todoapi/migrations/20251022201157_create_routines.sql
@@ -1,1 +1,10 @@
--- Add migration script here
+CREATE TABLE routines (
+    name VARCHAR(255),
+    created_by INTEGER REFERENCES users(id),
+    description TEXT,
+    PRIMARY KEY (name, created_by)
+)
+
+CREATE INDEX idx_routines_created_by ON routines(created_by);
+
+--TODO: maybe create an index to allow searching routines by equipments
